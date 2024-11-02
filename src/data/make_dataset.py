3fda7d8299537a2bd3c850329541311c8792213d
StephaneMartinez <stephane.martinez@netcourrier.com>
2024-11-02 18:20:03 +0000

    Replacing values -1 and 0

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -52,13 +52,13 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     df_users.drop(['an_nais'], inplace=True, axis=1)
 
         #--Replacing names 
-#    df_users.grav.replace([1,2,3,4], [1,3,4,2], inplace = True)
-#    df_caract.rename({"agg" : "agg_"},  inplace = True, axis = 1)
-#    corse_replace = {"2A":"201", "2B":"202"}
-#    df_caract["dep"] = df_caract["dep"].str.replace("2A", "201")
-#    df_caract["dep"] = df_caract["dep"].str.replace("2B", "202")
-#    df_caract["com"] = df_caract["com"].str.replace("2A", "201")
-#    df_caract["com"] = df_caract["com"].str.replace("2B", "202")
+    df_users.grav.replace([1,2,3,4], [1,3,4,2], inplace = True)
+    df_caract.rename({"agg" : "agg_"},  inplace = True, axis = 1)
+    corse_replace = {"2A":"201", "2B":"202"}
+    df_caract["dep"] = df_caract["dep"].str.replace("2A", "201")
+    df_caract["dep"] = df_caract["dep"].str.replace("2B", "202")
+    df_caract["com"] = df_caract["com"].str.replace("2A", "201")
+    df_caract["com"] = df_caract["com"].str.replace("2B", "202")
 
     #--Converting columns types
 #    df_caract[["dep","com", "hour"]] = df_caract[["dep","com", "hour"]].astype(int)
